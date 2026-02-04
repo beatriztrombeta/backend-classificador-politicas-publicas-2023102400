63175e09df8aaa39bce00e5a90a44d3b1241542b
beatriztrombeta <beatriztrombeta@gmail.com>
2026-02-04 08:54:23 -0300

    Added CORS

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
+from app.database import Base, engine
 
-load_dotenv()
+app = FastAPI(title="Classificador de Políticas Públicas")
 
-app = FastAPI(
-    title="Classificador de Políticas Públicas",
-    version="0.1.0"
+Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
